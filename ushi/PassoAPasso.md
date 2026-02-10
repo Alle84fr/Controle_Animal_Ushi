8bbcbaeca1c5856c8e884520e0fe374333046f2e
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2026-02-10 14:11:13 -0300

    primeiro commit

---
+++ ushi/PassoAPasso.md
@@ -0,0 +1,9 @@
+<h3>1° comando</h3>
+
+npx create-react-app Projeto_Ushi
+criar o projeto
+
+cd Ushi para entrar
+
+para rodar
+npx start
---
+++ ushi/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+import logVaca from "./imgs/ushi_logo.png"
+
+
+function App() {
+  return (
+    <div className="App">
+
+            <img src={logVaca} alt="Imagem vaca vista por cima"/>
+  
+    </div>
+  );
+}
+
+export default App;
